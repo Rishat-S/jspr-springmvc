79f10296da7cb44b8d4c5ffd49e1718dce1de5e6
Rishat Suleymanov <spk_irbis@mail.ru>
2022-02-04 14:48:23 +0300

    Refactoring

--- src/main/java/ru/netology/controller/PostController.java
+++ src/main/java/ru/netology/controller/PostController.java
@@ -4,39 +4,42 @@ import org.springframework.web.bind.annotation.*;
 import ru.netology.model.Post;
 import ru.netology.service.PostService;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/post")
 public class PostController {
-  private final PostService service;
+    private final PostService service;
 
-  public PostController(PostService service) {
-    this.service = service;
-  }
-
-  @GetMapping("/all")
-  public List<Post> all() {
-    return service.all();
-  }
-
-  @GetMapping("/{id}")
-  public Post getById(@PathVariable long id) {
-    return service.getById(id);
-  }
-
-  @PostMapping
-  public Post save(@RequestBody Post post) {
-
-    if (post.getId() == 0) {
-      return service.save(post);
-    } else {
-      return service.update(post);
+    public PostController(PostService service) {
+        this.service = service;
     }
-  }
 
-  @DeleteMapping("/{id}")
-  public void removeById(@PathVariable long id) {
-    service.removeById(id);
-  }
+    @GetMapping("/all")
+    public List<Post> all() {
+        return service.all();
+    }
+
+    @GetMapping("/{id}")
+    public Post getById(@PathVariable long id) {
+        return service.getById(id);
+    }
+
+    @PostMapping
+    public Post save(@RequestBody Post post, HttpServletResponse response) {
+
+        if (post.getId() == 0) {
+            response.setStatus(201);
+            return service.save(post);
+        } else {
+            response.setStatus(200);
+            return service.update(post);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public void removeById(@PathVariable long id) {
+        service.removeById(id);
+    }
 }
